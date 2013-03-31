2850f64c3b99f5b552ddbee633c8067bbf328a23
w. chorolque/n <wchorolque@gmail.com>
2013-03-31 16:03:52 -0400

    initial commit

---
+++ mainConsole/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace mainConsole
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
