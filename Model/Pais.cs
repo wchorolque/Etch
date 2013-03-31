714d0c61018330f75765d1345a8ae7ddd7c7d108
w. chorolque <wchorolque@gmail.com>
2013-03-31 16:46:30 -0400

    Creado el modelo pais

--- Model/Pais.cs
+++ Model/Pais.cs
@@ -4,8 +4,23 @@ namespace Model
 {
     public class Pais
     {
+        public string Codigo {
+            get;
+            set;
+        }
+
+        public string Nombre {
+            get;
+            set;
+        }
+
         public Pais ()
         {
+        }
+
+        public override string ToString ()
+        {
+            return Nombre;
         }
     }
 }
